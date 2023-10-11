e04b30dc5af91d91133ff5caadad5d908a7103cc
mahj79 <mahonej79@gmail.com>
2023-10-11 10:21:10 -0500

    code for Dijkstra's alog practice

---
+++ shortestPath.js
@@ -0,0 +1,22 @@
+const testGraph = require('./testGraph.js');
+const dijkstras = require('./dijkstras.js');
+
+const shortestPathBetween = (graph, startingVertex, targetVertex) => {
+ const {distances, previous} = dijkstras(graph, startingVertex);
+  const distance = distances[targetVertex.data];
+  const path = [];
+  let vertex = targetVertex;
+  while(vertex !== null){
+    path.unshift(vertex);
+    vertex = previous[vertex.data];
+  }
+  return {distance: distance, path};
+};
+
+// Retrieve shortest path between vertices A and G
+const a = testGraph.getVertexByValue('A');
+const g = testGraph.getVertexByValue('G');
+const results = shortestPathBetween(testGraph, a, g);
+
+module.exports = shortestPathBetween;
+console.log(results);
